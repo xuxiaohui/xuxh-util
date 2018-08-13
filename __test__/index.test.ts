acf92bb2a9b083176c27bbf1e174f28f1523bea2
xu_xiaohui7685@126.com <xu_xiaohui7685@126.com>
2018-08-13 20:38:48 +0800

    添加新的方法

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,10 +1,9 @@
 import { expect } from "chai";
 import "mocha";
-import { getTradeUrl } from "../index";
+import * as tool from "../index";
 
 describe("测试", () => {
-  it("test getTradeUrl", () => {
-    let testStr1 = `周末福利
+  let testStr1 = `周末福利
         给〖孕期妈妈〗
         525孕妇照免费拍（仅剩99个名额）
         「报名」http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3426
@@ -32,7 +31,48 @@ describe("测试", () => {
 还有博览会的链接哦
 http://szmuyingzhan.ihealthcoming.com/conference/views/success.html?userId=298&channel=yuejk&date=2018-07-30&vHMC=66&vHMP=szyueer1&isYun=true还哦哦哦`;
 
-    let arr = getTradeUrl(testStr1);
+  let testStr3 = `周末福利
+        给〖孕期妈妈〗
+        525孕妇照免费拍（仅剩99个名额）
+        「报名」http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3426
+        
+        孕妈运动训练，专属一对一私教体验课：
+        「体验」http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3581
+        
+        三甲名医四维彩超，给宝宝的第一张【照片】
+        预约免排队，还有独！家！特！惠！
+        「参与」?http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=2713
+        
+        给〖产后妈妈〗
+        蓝丝带产后修复服务，体验过的妈妈说很舒服哦[奸笑]
+        （骨盆康复、子宫复旧、温宫散寒……….）
+        「体验」http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3436
+        
+        〖一起来〗
+        年度“孕妇节”盛典邀您参与，有豪礼?
+        （同时招募表演嘉宾+观众哦）
+        「报名」https://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3618
+        
+        测试链接来了
+        https://webchat.baymy.cn/yuezicenter/views/yuezi_detail.html?id=3323
+        http://webchat.baymy.cn/yuedoctor/views/morning_paper.html?id=3770
+        `
+  let testStr2 = `
+        ?羊城首家专注产后运动恢复塑身馆苏晴教练『骨盆修复』小班体验课 ✚ 赠1次骨盆检测，戳▼
+        
+        http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3723
+        
+        『产后收腹』小班课仍可报名，戳▼
+        
+        http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3453
+        
+        ?全国首家李欣普拉提孕动主题店李华敏团队产后运动修复体验课（私教），戳▼
+        
+        http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3535`
+
+  it("test getTradeUrl", () => {
+    
+    let arr = tool.getTradeUrl(testStr1);
     expect(arr.length).to.be.equal(8);
     expect(arr).to.include(
       "http://webchat.baymy.cn/yuedoctor/views/morning_paper.html?id=3770"
@@ -59,4 +99,100 @@ http://szmuyingzhan.ihealthcoming.com/conference/views/success.html?userId=298&c
       "http://szmuyingzhan.ihealthcoming.com/conference/views/success.html?userId=298&channel=yuejk&date=2018-07-30&vHMC=66&vHMP=szyueer1&isYun=true"
     );
   });
+  it('getTradeUrl with https', () => {
+    let arr = tool.getTradeUrl(testStr3)
+    expect(arr.length).to.be.equal(7)
+    expect(arr).to.include('http://webchat.baymy.cn/yuedoctor/views/morning_paper.html?id=3770')
+    expect(arr).to.include('https://webchat.baymy.cn/yuezicenter/views/yuezi_detail.html?id=3323')
+    expect(arr).to.include('https://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3618')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=2713')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3581')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3426')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3436')
+  })
+  it('test getTradeUrls', () => {
+    let arr = tool.getTradeUrls(testStr1, testStr2)
+    expect(arr.length).to.be.equal(11)
+    expect(arr).to.include('http://webchat.baymy.cn/yuedoctor/views/morning_paper.html?id=3770')
+    expect(arr).to.include('https://webchat.baymy.cn/yuezicenter/views/yuezi_detail.html?id=3323')
+    expect(arr).to.include('https://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3618')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=2713')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3581')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3426')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3436')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3723')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3453')
+    expect(arr).to.include('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3535')
+    expect(arr).to.include('http://szmuyingzhan.ihealthcoming.com/conference/views/success.html?userId=298&channel=yuejk&date=2018-07-30&vHMC=66&vHMP=szyueer1&isYun=true')
+  })
+  it('replaceAllForString', () => {
+    let testStr4 = `
+      ?羊城首家专注产后运动恢复塑身馆苏晴教练『骨盆修复』小班体验课 ✚ 赠1次骨盆检测，戳▼
+      
+      http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3723
+      
+      『产后收腹』小班课仍可报名，戳▼
+      
+      http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3453&vHMC=66&vHMP=yueer
+      http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3453&vHMC=66&vHMP=yueer
+      
+      ?全国首家李欣普拉提孕动主题店李华敏团队产后运动修复体验课（私教），戳▼
+      
+      http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3535`
+
+    let targetStr = tool.replaceAllForString(testStr4, 'http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3453&vHMC=66&vHMP=yueer', 'http://www.baidu.com')
+
+    expect(targetStr).to.be.equal(`
+      ?羊城首家专注产后运动恢复塑身馆苏晴教练『骨盆修复』小班体验课 ✚ 赠1次骨盆检测，戳▼
+      
+      http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3723
+      
+      『产后收腹』小班课仍可报名，戳▼
+      
+      http://www.baidu.com
+      http://www.baidu.com
+      
+      ?全国首家李欣普拉提孕动主题店李华敏团队产后运动修复体验课（私教），戳▼
+      
+      http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3535`)
+  })
+
+  it('isOurWeixinURL', () => {
+    expect(tool.isOurWeixinURL('http://www.baidu.com')).to.be.false
+    expect(tool.isOurWeixinURL('http://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3535')).to.be.true
+    expect(tool.isOurWeixinURL('https://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3535')).to.be.true
+    expect(tool.isOurWeixinURL('http://webchat.baymy.cn/yuezicenter/views/yuezi_detail.html?id=3323')).to.be.true
+    expect(tool.isOurWeixinURL('https://webchat.baymy.cn/yuezicenter/views/yuezi_detail.html?id=3323')).to.be.true
+  });
+  it('getUrlIds', () => {
+    // 商品详情
+    let activityUrlIds = tool.getUrlIds('https://webchat.ihealthcoming.com/webchatpay/views/ecshop_detail.html?ativityId=3535')
+    expect(activityUrlIds.length).to.be.equal(1)
+    expect(activityUrlIds).to.include(3535)
+
+    // 健康早报
+    let morningPaperIds = tool.getUrlIds('http://webchat.baymy.cn/yuedoctor/views/morning_paper.html?id=3770')
+    expect(morningPaperIds.length).to.be.equal(1)
+    expect(morningPaperIds).to.include(3770)
+
+    //健康知识库
+    let knowledgeIds = tool.getUrlIds('https://webchat.ihealthcoming.com/knowledgebase/views/knowledge_detail.html?bookId=4184&vHMC=11&vHMP=youer&vNum=1&vLAST=youer')
+    expect(knowledgeIds.length).to.be.equal(1)
+    expect(knowledgeIds).to.include(4184)
+
+    //月子中心详情
+    let yeuziIds = tool.getUrlIds('http://webchat.baymy.cn/yuezicenter/views/yuezi_detail.html?id=3323')
+    expect(yeuziIds.length).to.be.equal(1)
+    expect(yeuziIds).to.include(3323)
+
+    // 健康活动
+    let welfareIds = tool.getUrlIds('http://webchat.ihealthcoming.com/welfare/views/welfare_detail.html?id=3618')
+    expect(welfareIds.length).to.be.equal(1)
+    expect(welfareIds).to.include(3618)
+
+    let classH5Ids = tool.getUrlIds('https://webchat.ihealthcoming.com/miniProgramShare/views/classShare.html?vHMC=66-40093-2&vHMP=yueer4&classId=97&classType=1&chapterId=1523&from=timeline&isappinstalled=0&followAssistant=1')
+    expect(classH5Ids.length).to.be.equal(2)
+    expect(classH5Ids).to.include(97)
+    expect(classH5Ids).to.include(1523)
+  })
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-export function getTradeUrl(str: String): Array<String> {
-  let urlArr: Array<String> = [];
-  let regPro = /(http(s?):\/\/(\w+.ihealthcoming.com|\w+.baymy.cn)[A-Za-z0-9_\-/.?=&;#]*)/g
+/**
+ * 获取页面中对应域名的所有链接
+ * @param str 
+ */
+export function getTradeUrl(str: string): string[] {
+  let urlArr: string[] = [];
+  let regPro = /(http(s?):\/\/(\w+.ihealthcoming.com|\w+.baymy.cn)[A-Za-z0-9_\-/.?=&;#]*)/g;
   let element = checkHtmlStr(str);
   if (element) {
     (element.match(regPro) || []).forEach(function(item) {
@@ -13,8 +17,90 @@ export function getTradeUrl(str: String): Array<String> {
 }
 
 /**
+ * 提取对应文本中的有效链接地址
+ * @param contents 需要处理的
+ * @return {*}
+ */
+export function getTradeUrls(...contents:string[]): string[] {
+  let arr: string[] = [];
+  for (let content of contents) {
+    arr.push(content);
+  }
+  return getTradeUrlsForArr(arr);
+}
+
+export function getTradeUrlsForArr(arr: string[]): string[] {
+  let retArr: string[] = [];
+  arr.forEach(item => {
+    retArr = retArr.concat(getTradeUrl(item));
+  });
+  return retArr;
+}
+
+/**
+ * 更改url中某个参数的值
+ * @param url 待更改的url链接
+ * @param arg 参数名称
+ * @param arg_val 参数值
+ */
+export function changeURLArg(url: string, arg: string, arg_val: string) {
+  let pattern = arg + "=([^&]*)";
+  let replaceText = arg + "=" + arg_val;
+  if (url.match(pattern)) {
+    let tmp = url.replace(
+      new RegExp("(" + arg + "=)([^&]*)", "gi"),
+      replaceText
+    );
+    return tmp;
+  } else {
+    if (url.match("[?]")) {
+      return url + "&" + replaceText;
+    } else {
+      return url + "?" + replaceText;
+    }
+  }
+}
+
+/**
+ * 替换字符串
+ * @param content 待替换的字符串
+ * @param tradeStr 被替换的字符串
+ * @param targetStr 将要替换成的字符串
+ */
+export function replaceAllForString(
+  content: string,
+  tradeStr: string,
+  targetStr: string
+): string {
+  tradeStr = tradeStr.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  return content.replace(new RegExp(tradeStr, "g"), targetStr);
+}
+
+/**
+ * 判断链接是不是我们域名的url
+ * @param url
+ */
+export function isOurWeixinURL(url: string): boolean {
+  let regPro = /(http(s?):\/\/(\w+.ihealthcoming.com|\w+.baymy.cn)[A-Za-z0-9_\-/.?=&;#]*)/g;
+  return regPro.test(url);
+}
+
+/**
+ * 从给定的url中提取出对应的id值
+ * @param url
+ */
+export function getUrlIds(url: string): number[] {
+  let reg = /id=([0-9]+)/gi;
+  let ids: number[] = [];
+  while (reg.test(url)) {
+    ids.push(parseInt(RegExp.$1));
+  }
+  return ids;
+}
+
+/**
  * 对链接中需要转义的html符号进行转义
- * @param str 
+ * @param str
  */
 function checkHtmlStr(str: String): String {
   let s = "";
